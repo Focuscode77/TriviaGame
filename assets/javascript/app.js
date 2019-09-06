5fe70fffd1e2b7cc70ebd0cfdb43b2bae575b1fd
Joshua Coleman <procoleman77@outlook.com>
2019-09-06 17:43:22 -0400

    added resluts page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,7 @@
 var count = 10;
 var currentQuestion = 0;
 var score = 0;
-var lost = 0;
+var lose = 0;
 var result = '';
 //  Variable that will hold our interval ID when we execute
 //  the "run" function
@@ -52,12 +52,13 @@ $(document).ready(function () {
         $("#Start").hide();
 
 
-        function newQuestion (){
-         var isQuestionOver = (questionArr.length -1)
-           
-         if(isQuestionOver === currentQuestion){
-             console.log("Game over")
-            }else{
+        function newQuestion() {
+            var isQuestionOver = (questionArr.length - 1)
+
+            if (isQuestionOver === currentQuestion) {
+                console.log("Game over")
+                displayResult()
+            } else {
 
                 currentQuestion++;
                 startQuestion()
@@ -72,52 +73,90 @@ $(document).ready(function () {
             count--;
             // Log/Debugger
             console.log(count)
-            //  Show the number in the #show-number tag.
-            $("#show-number").html("<h3>" + "Time Reamining:" + "" + count + "</h3>");
             if (count === 0) {
-                //  ...run the stop function.
-                
+
                 //  Alert the user that time is up.
                 timeUp();
             }
+            //  Show the number in the #show-number tag.
+            $("#show-number").html("<h3>" + "Time Reamining:" + "" + count + "</h3>");
+
 
         };
+
+
         function timeUp() {
             //  to the clearInterval function.
             clearInterval(intervalId);
-            lost++;
+            lose++;
             newQuestion();
         }
+
+
         // Function for questions
         function startQuestion() {
-
             var question = questionArr[currentQuestion].question;
             var choices = questionArr[currentQuestion].choices;
             // Check debug
             console.log(question)
             console.log(choices)
+
+
+            count = 10;
+            intervalId = setInterval(decrement, 1000);
+
             $("#game").html(`<h4> ${question} </h4> ${loadChoices(choices)}`)
-     
+
             // $("#answer").html('<p>' + choices + '</p>'
 
         };
 
         // Function for choices(and timer)
         function loadChoices(choices) {
-            count = 10;
-         intervalId = setInterval(decrement, 1000);
-          var result = '';
+            var result = '';
             for (let i = 0; i < choices.length; i++) {
 
                 result += `<p class="choice" data-answer="${choices[i]}">${choices[i]} </p>`;
-// Check Debug
-               console.log(result)
+                // Check Debug
+                console.log(result)
 
             }
             return result;
 
         };
-        
+
+        // Go to next question rather right or wrong
+        $(document).on('click', '.choice', function () {
+            clearInterval(intervalId);
+            const selectedAnswer = $(this).attr('data-answer');
+            var rightAnswer = questionArr[currentQuestion].correctAnswer;
+            if (rightAnswer === selectedAnswer) {
+                // User win
+                score++;
+                newQuestion();
+                console.log("Win");
+
+            } else {
+                lose++;
+                newQuestion();
+                console.log("loss");
+            }
+
+            console.log('period', selectedAnswer);
+
+        });
+
+        // display
+        function displayResult() {
+            const result = `
+              <p> You got ${score} question right! </p>
+              <p> You missed ${lose} questions! </p>
+              <p> Total questions ${questionArr.length}! </p>
+                 <button class="btn btn-primary" id = reset> Reset Game </button>
+            `;
+
+            $('#game').html(result);
+        }
 
         startQuestion();
 
